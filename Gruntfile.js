c26bb9128958799afb85abb2b964db22ad657f12
Mike Wadsten <mikewadsten@gmail.com>
2013-04-27 17:11:56 -0500

    Change Gruntfile to clean public/ directory and rebuild it from index.html, assets, and coffeescript output.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,11 +2,13 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
-        clean: ["public/assets/rocksinspace.*"],
+        clean: ["public"],
 
         coffee: {
-            joined: {
-                options: {join: true},
+            options: {
+                join: true
+            },
+            rocksinspace: {
                 files: {
                     'coffee/rocksinspace.js': ['coffee/*.coffee']
                 }
@@ -22,16 +24,41 @@ module.exports = function(grunt) {
             rocksinspace: {
                 files: {'public/assets/rocksinspace.min.js': ['coffee/rocksinspace.js']}
             }
+        },
+
+        htmlmin: {
+            options: {
+                removeComments: true,
+                collapseWhitespace: true
+            },
+            rocksinspace: {
+                files: {
+                    "public/index.html": "index.html"
+                }
+            }
+        },
+
+        copy: {
+            deploy: {
+                files: [
+                    {flatten: true, expand: true, src: ['assets/*'], dest: 'public/assets/'}
+                ]
+            }
         }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-coffee');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
     grunt.registerTask('default', "My default task.", function() {
-        grunt.log.writeln("Generating rocksinspace.min.js using Coffeescript and uglify...");
         grunt.task.run('clean');
+        grunt.log.writeln("Generating rocksinspace.min.js using Coffeescript and uglify...");
         grunt.task.run(['coffee', 'uglify']);
+        grunt.log.writeln("Generating minified HTML files...");
+        grunt.task.run('htmlmin');
+        grunt.task.run('copy');
     });
 }
