bfb064fef299b0c47054b6a71e607b0cf019c274
Mike Wadsten <mikewadsten@gmail.com>
2013-04-27 17:50:08 -0500

    Update server to log requests and stuff

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 var connect = require('connect'),
+    fs = require('fs'),
     http = require('http');
 
 var app = connect()
+    .use(connect.logger({format: 'short', stream: fs.createWriteStream("access.log")}))
     .use(connect.static('public'))
+    //.use(connect.logger('tiny'))
     .use(function(req, res) {
         res.end('Hello from my little test server!\n')
     });
